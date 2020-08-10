3ab680056cdc2aa110d04314ecfcaa2e06f0acf6
Shane Brunswick <shanebru62@gmail.com>
2020-08-09 22:04:49 -0700

    Text reversion

--- _site/index.js
+++ _site/index.js
@@ -405,10 +405,10 @@ function gameStateF() {
 
         if (isMobile){
           textSize(window.innerWidth * .1);
-          text("WEAR A MASK,\nWIN 2021.\n\nSPREAD THE WORD\n(not the virus)\n\n#WearMaskWin21", window.innerWidth/2, window.innerHeight/2);
+          text("WEAR A MASK,\nWIN 2021.\n\n#WearMaskWin21\n\nSPREAD THE WORD\n(not the virus)", window.innerWidth/2, window.innerHeight/2);
         } else {
           textSize(window.innerWidth * .052);
-          text("WEAR A MASK,\nWIN 2021.\n\nSPREAD THE WORD\n(not the virus)\n\n#WearMaskWin21", window.innerWidth/2, window.innerHeight/2);
+          text("WEAR A MASK,\nWIN 2021.\n\n#WearMaskWin21\n\nSPREAD THE WORD\n(not the virus)", window.innerWidth/2, window.innerHeight/2);
         }
 
 
