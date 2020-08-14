2f65102240a95268bd8e29fc472bfe259d291f4c
Shane Brunswick <shanebru62@gmail.com>
2020-08-14 07:00:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -461,13 +461,13 @@ function gameStateF() {
     textSize(30);
     text("Score: "+hero.health, (window.innerWidth/2), 185);
 
-    textSize(16);
+    textSize(21);
 
     if (!isMobile) {
-      textSize(15.5)
+      textSize(21)
     }
 
-    text("onedayin2019.com", (window.innerWidth/2), window.innerHeight - 23);
+    text("onedayin2019.com", (window.innerWidth/2), window.innerHeight - 35);
 
 
     if (clockScore > 12 && onlyOnceFlag == false) {
